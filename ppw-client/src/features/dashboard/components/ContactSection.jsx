ccaf1c45e0316223dbcbc7546c1d298b0aab7d1b
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-10-19 16:20:43 +0530

    loader implemented in dashboard

--- ppw-client/src/features/dashboard/components/ContactSection.jsx
+++ ppw-client/src/features/dashboard/components/ContactSection.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import DefaultAvatar from '../../profile/DefaultAvatar';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { resolveContactQueryAsync } from '../dashboardSlice';
+import { LoaderIcon } from 'react-hot-toast';
 
 export default function ContactSection({ contacts }) {
   const dispatch = useDispatch();
+
+  const { status } = useSelector((state) => state.dashboard);
 
   return (
     <div className=" w-full border-2 border-gray-300 p-2 rounded">
@@ -12,52 +15,58 @@ export default function ContactSection({ contacts }) {
         <span className="font-semibold">Contacts:</span>
         {contacts.length}
       </h2>
-      <ul role="list" className="divide-y divide-gray-100 w-fit ">
-        {contacts.map((contact) => (
-          <li
-            key={contact._id}
-            className="flex items-start justify-start gap-x-6 py-5 px-2 w-fit sm:w-full  hover:bg-gray-200"
-            title={contact.message}
-          >
-            <DefaultAvatar
-              name={contact.name}
-              classes={`w-12 h-12 text-lg font-semibold`}
-            />
-            <div className="flex flex-wrap justify-between w-[90%] min-w-0 gap-x-4">
-              <div className="min-w-0 flex-auto sm:w-3/4 ">
-                <p className="text-sm font-semibold leading-6 text-gray-900 flex items-center ">
-                  <span>{contact.name}</span>
-                  <span className="ml-2 rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                    {contact.university}
-                  </span>
-                </p>
-                <p className="mt-1  text-sm leading-5 text-gray-500">
-                  {contact.message.length > 100
-                    ? `${contact.message.slice(0, 100)}...`
-                    : contact.message}
-                </p>
+      {status === 'loading' ? (
+        <div className="w-full flex items-center justify-center p-2">
+          <LoaderIcon />
+        </div>
+      ) : (
+        <ul role="list" className="divide-y divide-gray-100 w-fit ">
+          {contacts.map((contact) => (
+            <li
+              key={contact._id}
+              className="flex items-start justify-start gap-x-6 py-5 px-2 w-fit sm:w-full  hover:bg-gray-200"
+              title={contact.message}
+            >
+              <DefaultAvatar
+                name={contact.name}
+                classes={`w-12 h-12 text-lg font-semibold`}
+              />
+              <div className="flex flex-wrap justify-between w-[90%] min-w-0 gap-x-4">
+                <div className="min-w-0 flex-auto sm:w-3/4 ">
+                  <p className="text-sm font-semibold leading-6 text-gray-900 flex items-center ">
+                    <span>{contact.name}</span>
+                    <span className="ml-2 rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                      {contact.university}
+                    </span>
+                  </p>
+                  <p className="mt-1  text-sm leading-5 text-gray-500">
+                    {contact.message.length > 100
+                      ? `${contact.message.slice(0, 100)}...`
+                      : contact.message}
+                  </p>
+                </div>
+                <div className=" shrink-0 sm:flex sm:flex-col sm:items-end border-t-2 border-gray-400 pr-4 sm:pr-0 sm:border-none">
+                  <p className="text-xs text-gray-500 leading-6 ">
+                    {contact.createdAt}
+                  </p>
+                  {
+                    <div className="mt-1 flex items-center  gap-x-1.5">
+                      <button
+                        onClick={() =>
+                          dispatch(resolveContactQueryAsync(contact._id))
+                        }
+                        className="text-sm font-semibold bg-green-800 text-white px-2 py-[2px] rounded hover:bg-green-700"
+                      >
+                        Resolve
+                      </button>
+                    </div>
+                  }
+                </div>
               </div>
-              <div className=" shrink-0 sm:flex sm:flex-col sm:items-end border-t-2 border-gray-400 pr-4 sm:pr-0 sm:border-none">
-                <p className="text-xs text-gray-500 leading-6 ">
-                  {contact.createdAt}
-                </p>
-                {
-                  <div className="mt-1 flex items-center  gap-x-1.5">
-                    <button
-                      onClick={() =>
-                        dispatch(resolveContactQueryAsync(contact._id))
-                      }
-                      className="text-sm font-semibold bg-green-800 text-white px-2 py-[2px] rounded hover:bg-green-700"
-                    >
-                      Resolve
-                    </button>
-                  </div>
-                }
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- ppw-client/src/features/dashboard/components/UsersSection.jsx
+++ ppw-client/src/features/dashboard/components/UsersSection.jsx
@@ -1,52 +1,64 @@
+import { useSelector } from 'react-redux';
 import DefaultAvatar from '../../profile/DefaultAvatar';
 import { CheckBadgeIcon } from '@heroicons/react/24/outline';
+import { LoaderIcon } from 'react-hot-toast';
 
 export default function UsersSection({ users }) {
+  const { status } = useSelector((state) => state.dashboard);
+
   return (
     <div className=" w-full border-2 border-gray-300 p-2 rounded mb-4">
       <h2 className="text-xl">
         <span className="font-semibold">Total users:</span> {users.length}
       </h2>
-      <ul role="list" className="divide-y divide-gray-100 w-fit ">
-        {users.map((user) => (
-          <li
-            key={user.email}
-            className="flex justify-between px-2 gap-x-6 py-5"
-          >
-            <div className="flex min-w-0 gap-x-4">
-              <DefaultAvatar
-                name={user.name}
-                classes={`w-12 h-12 text-lg font-semibold`}
-              />
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900 flex items-center ">
-                  <span>{user.name}</span>
+      {status === 'loading' ? (
+        <div className="w-full flex items-center justify-center p-2">
+          <LoaderIcon />
+        </div>
+      ) : (
+        <ul role="list" className="divide-y divide-gray-100 w-fit ">
+          {users.map((user) => (
+            <li
+              key={user.email}
+              className="flex justify-between px-2 gap-x-6 py-5"
+            >
+              <div className="flex min-w-0 gap-x-4">
+                <DefaultAvatar
+                  name={user.name}
+                  classes={`w-12 h-12 text-lg font-semibold`}
+                />
+                <div className="min-w-0 flex-auto">
+                  <p className="text-sm font-semibold leading-6 text-gray-900 flex items-center ">
+                    <span>{user.name}</span>
 
-                  {user.isEmailVerified && (
-                    <CheckBadgeIcon
-                      className="w-5 h-5 ml-1 text-indigo-700 font-bold"
-                      title="Verified"
-                    />
-                  )}
-                </p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                  {user.email}
-                </p>
-              </div>
-            </div>
-            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm leading-6 text-gray-900">{user.username}</p>
-              {
-                <div className="mt-1 flex items-center gap-x-1.5">
-                  <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                    {user.role}
-                  </span>
+                    {user.isEmailVerified && (
+                      <CheckBadgeIcon
+                        className="w-5 h-5 ml-1 text-indigo-700 font-bold"
+                        title="Verified"
+                      />
+                    )}
+                  </p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                    {user.email}
+                  </p>
                 </div>
-              }
-            </div>
-          </li>
-        ))}
-      </ul>
+              </div>
+              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <p className="text-sm leading-6 text-gray-900">
+                  {user.username}
+                </p>
+                {
+                  <div className="mt-1 flex items-center gap-x-1.5">
+                    <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                      {user.role}
+                    </span>
+                  </div>
+                }
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
